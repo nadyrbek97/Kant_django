e5625fa307a906c0c0466e73fa10a68678413609
nadyrbek97 <nadyrbek97@gmail.com>
2019-01-30 21:23:40 +0600

    serializers added

---
+++ kant/expenses/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from .models import (FieldsModel,
+                     FieldExpenses,
+                     SugarBeetPointModel,
+                     Expenses,
+                     CommodityModel,)
+
+
+class FieldsModelSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FieldsModel
+        fields = ('beet_point', 'id', 'year', 'average_harvest', 'field_id', 'hectares', )
+
+
+class ExpensesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Expenses
+        fields = ('id', 'name', 'price', 'amount')
+
+
+class FieldExpensesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = FieldExpenses
+        fields = ('id', 'name', 'price', 'amount')
+
+
+class SugarBeetPointSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = SugarBeetPointModel
+        fields = ('id', 'name')
